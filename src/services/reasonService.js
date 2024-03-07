b86063b58cf4795bf812fe0d337bc918b070e1da
dthabata <dthabatita@gmail.com>
2024-03-07 10:45:58 -0300

    feat: testing project

--- src/services/reasonService.js
+++ src/services/reasonService.js
@@ -3,7 +3,7 @@ const reasonService = {
         try {
             let response = await fetch('https://mocki.io/v1/f42fca53-2029-4808-b429-edf21834d002')
             return await response.json()
-            } catch (error) {
+        } catch (error) {
                 console.error(error);
                 return [];
             }
